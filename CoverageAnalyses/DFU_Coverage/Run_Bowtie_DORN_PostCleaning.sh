e996c5099ade0b22efa82a2856329895861d76bc
Amo <amyecampbell@gmail.com>
2021-02-15 13:04:57 -0500

    adding required bowtie running script and admixture output R script

---
+++ CoverageAnalyses/DFU_Coverage/Run_Bowtie_DORN_PostCleaning.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Shell script to map each DORN genome's raw sequencing reads 
+# Back to the assembled + cleaned contigs for that genome
+# for use downstream in TestAdmixtures.sh
+
+source /home/acampbe/software/miniconda3/bin/activate BowtieEnv
+
+export BOWTIE2_INDEXES=/home/acampbe/DFU/data/AlignmentCoverage/BowtieDatabase/
+
+# Build bowtie2 databases for every genome in the assembly folder
+outputfolder="/home/acampbe/DFU/data/AlignmentCoverage/DORN_Assembly_Coverages_Cleaned/"
+
+mkdir -p $outputfolder
+
+# Decided to use trimmed instead
+Raw_FastQs="/project/grice/storage/HiSeq/WGS/HiSeq_19/TrimmedFastqs_TrimGalore/"
+
+for filename in /project/grice/storage/HiSeq/WGS/HiSeq_19/AssemblyFastas/DFU100_Cleaned_Assemblies/FinalContigs/*.fasta; do 
+	filenamestring=$(basename $filename)
+	ext="_cleaned.fasta"
+	blank=""
+	noext=${filenamestring/$ext/$blank}
+	
+	bowtie2-build $filename $noext
+	mv *.bt2 /home/acampbe/DFU/data/AlignmentCoverage/BowtieDatabase/
+	
+	sam_ext=".sam"
+	fwd_Ext="trimmedgalore_val_1.fastq"
+	rev_Ext="trimmedgalore_val_2.fastq"
+
+
+	fwd_reads=$Raw_FastQs$noext$fwd_Ext
+	rev_reads=$Raw_FastQs$noext$rev_Ext
+
+	samfile=$noext$sam_ext
+	
+	bowtie2 -x $noext -1 $fwd_reads -2 $rev_reads -S $outputfolder$samfile
+
+done
