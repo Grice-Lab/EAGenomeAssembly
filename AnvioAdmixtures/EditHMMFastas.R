1cbe2adda78327dc657cf1c20751b43feb9d61c0
amyecampbell <amyecampbell@gmail.com>
2021-10-06 15:38:06 -0400

    hybrid assembly HMM edits

--- AnvioAdmixtures/EditHMMFastas.R
+++ AnvioAdmixtures/EditHMMFastas.R
@@ -27,4 +27,15 @@ AllNames = (AllNames[sapply(AllNames, function(x) !grepl(x, pattern=r))])
 
 # Hybrid assemblies 
 ###################
-c("SecY", "SmpB", "SecG", "PGK", "SecE", "Ribosom_S12_S23", "Ribosomal_S7", "Ribosomal_L1", "tRNA-synt_1d", "Ribosomal_L32p", "Ribosomal_S9", "Ribosomal_S6", "Ribosomal_L13", "Pept_tRNA_hydro")
+inputfolder="/Users/amycampbell/Documents/LocalCopyReads/HMMOutputLongRead/"
+removeNames=c("SecY", "SmpB", "SecG", "PGK", "SecE", "Ribosom_S12_S23", "Ribosomal_S7", "Ribosomal_L1", "tRNA-synt_1d", "Ribosomal_L32p", "Ribosomal_S9", "Ribosomal_S6", "Ribosomal_L13", "Pept_tRNA_hydro")
+files <- list.files(inputfolder,pattern = ".fasta")
+for (f in files){
+  fastafile = read.fasta(paste0(inputfolder, f), forceDNAtolower = F)
+  AllNames = names(fastafile)
+  for(r in removeNames){
+    AllNames = (AllNames[sapply(AllNames, function(x) !grepl(x, pattern=r))])
+  }
+  fastafilesubset = fastafile[AllNames]
+  write.fasta(fastafilesubset,names(fastafilesubset), file=paste0(inputfolder,f))
+}
