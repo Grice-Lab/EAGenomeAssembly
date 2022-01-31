df3b76ff9fa757c1f908d137ce0789d69e82a060
amyecampbell <amyecampbell@gmail.com>
2022-01-31 12:25:13 -0500

    for use on LPC

--- ContaminationCoverageCheckDFU/CoverageAnalyses.R
+++ ContaminationCoverageCheckDFU/CoverageAnalyses.R
@@ -1,14 +1,17 @@
+# Updated Jan 2022
+# Make a plot of the coverages by genome, select contigs with coverage outside IQR for blast followup
+# Filter to contigs >100bp in length >10X mean coverage
 
 library(dplyr)
 library(ggplot2)
 library(tidyr)
-library(forcats)
 library(grid)
 
+coveragestatspath="/home/acampbe/DFU/data/AlignmentCoverage/CompleteCoverageData/AllCoverageStats.tsv"
+outputfatespath="/home/acampbe/DFU/data/AlignmentCoverage/CompleteCoverageData/ContigFilteringFates.csv"
 
-setwd("/Users/amycampbell/Desktop/GriceLabGit/EAGenomeAssembly/CoverageAnalyses/DFU_Coverage/CoverageJan22/")
 
-DFU_stats = data.frame(read.table("Jan22CoverageStats.tsv", header=T)) 
+DFU_stats = data.frame(read.table(coveragestatspath, header=T)) 
 DFU_stats = DFU_stats %>% filter(!grepl("_backup", identifer))
 DFU_statsDORNS = DFU_stats %>% filter(grepl("DORN", identifer))
 
@@ -16,15 +19,24 @@ DFU_statsDORNS = DFU_statsDORNS %>% separate(identifer, c("Genome", "ContigID"),
 DFU_statsDORNS = DFU_statsDORNS %>% group_by(Genome) %>% mutate(MedianContigLength=median(length))
 
 
-ggplot(DFU_statsDORNS, aes(x=fct_reorder(as.factor(Genome), mean_cov, median), y=log10(mean_cov+1),  fill=MedianContigLength*.001)) + scale_fill_gradient(low="white", high="darkred") + geom_boxplot(outlier.size=.1, outlier.color=NULL, outlier.fill=NULL) + xlab("Genome") + ylab("Log10-transformed Mean Coverage by Contig")  + labs(fill="Median Contig Length of Assembly (kb)") +  theme_bw() + ggtitle("DORN Genome Contig Coverages") + theme(legend.position="bottom", axis.text.x=element_text(angle=90), panel.grid.major = element_blank(),
-panel.grid.minor = element_blank(), legend.title=element_text(size=16), axis.title.x=element_text(size=16), axis.title.y=element_text(size=16), plot.title=element_text(size=20, hjust=.5, face="bold"))  +  geom_hline(yintercept = log10(101), color="#008B8B", linetype="dashed") + geom_hline(yintercept=log10(51),color="#008B8B",linetype="dashed" )
+CovgPlot = ggplot(DFU_statsDORNS, aes(x=fct_reorder(as.factor(Genome), mean_cov, median), y=log10(mean_cov+1),  fill=MedianContigLength*.001)) +
+  scale_fill_gradient(low="white", high="darkred") + geom_boxplot(outlier.size=.1, outlier.color=NULL, outlier.fill=NULL) +
+  xlab("Genome") + ylab("Log10-transformed Mean Coverage by Contig")  + labs(fill="Median Contig Length of Assembly (kb)") +
+  theme_bw() + ggtitle("DORN Genome Contig Coverages") +
+  theme(legend.position="bottom", axis.text.x=element_text(angle=90), panel.grid.major = element_blank(),
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                    panel.grid.minor = element_blank(), legend.title=element_text(size=16), axis.title.x=element_text(size=16), axis.title.y=element_text(size=16), plot.title=element_text(size=20, hjust=.5, face="bold"))  +  geom_hline(yintercept = log10(101), color="#008B8B", linetype="dashed") + geom_hline(yintercept=log10(51),color="#008B8B",linetype="dashed" )
+ggsave(CovgPlot, file="CoveragePlot.pdf")
 
 DFU_statsDORNS = DFU_statsDORNS %>% filter(ContigID !="*")
 
-# FIlter out contigs <500 bp in length 
-DFU_statsDORNS = DFU_statsDORNS %>% filter(length >= 500)
+# Filter out contigs <100 bp in length 
+print("Prefilter:")
+print(dim(DFU_statsDORNS))
 
+DFU_statsDORNS = DFU_statsDORNS %>% filter(length >= 100)
 
+print("Post-length-filter:")
+print(dim(DFU_statsDORNS))
 
 # Identify contigs with outlier depths for the genome as falling outside of IQR
 DFU_statsDORNS = DFU_statsDORNS %>% group_by(Genome) %>% mutate(LowCutoff = (median(mean_cov)-(1.5*IQR(mean_cov)))) 
@@ -43,7 +55,7 @@ DFU_statsDORNS = DFU_statsDORNS %>% mutate(SortContig = case_when(isLowOutlier ~
                                                                   
 ))
 
-write.csv(DFU_statsDORNS[c("identifer", "Genome", "ContigID", "SortContig")], file="ContigFilteringFates22.csv")
+write.csv(DFU_statsDORNS[c("identifer", "Genome", "ContigID", "SortContig")], file=outputfatespath)
 
 
 
